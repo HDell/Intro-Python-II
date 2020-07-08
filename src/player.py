36b8043a6dc7306292a4248abfa377bf1e6de16d
Handell Desulme <tue73811@temple.edu>
2020-07-08 18:18:09 -0400

    completes Day 1 MVP

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player:
+    def __init__(self, room):
+        self.room = room
+    def __str__(self):
+        return "Your location is: " + self.room.name + ".\n" + self.room.description
+    
